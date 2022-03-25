efcd0505631b3bf5058e4c2cb0d5e4fe55dfec75
SimonBoerner <boernersimon@gmail.com>
2022-03-25 14:18:30 +0100

    added fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}", "./index.html"],
   theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      ptserif: ["PT Serif", "serif"],
+    },
     extend: {},
   },
   plugins: [],
