9c57abde4583e006aad2fc71abcf4c1e19026fd9
nort4n <trikkster@gmail.com>
2022-03-28 10:48:24 +0200

    added Chakra UI dependency

--- frontend/src/layout/Header.tsx
+++ frontend/src/layout/Header.tsx
@@ -1,2 +1,15 @@
 import React from "react";
+import
 
+function MyComp() {
+  // Good! Multiline wrapped in ( ... )
+  // also has a single top level node.
+  return ( // <- (
+    <div>
+      <h1>Hello</h1>
+      <p>World</p>
+    </div>
+  ) // <- )
+}
+
+export default MyComp
