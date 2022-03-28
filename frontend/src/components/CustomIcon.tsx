db3a8f40590d86390f4bafbbadf382dec9f40466
nort4n <trikkster@gmail.com>
2022-03-28 16:21:22 +0200

    added CustomIcon

---
+++ frontend/src/components/CustomIcon.tsx
@@ -0,0 +1,14 @@
+import { Link, LinkProps, useMatch, useResolvedPath } from "react-router-dom";
+
+const CustomIcon = ({ to, children }: LinkProps) => {
+  let resolved = useResolvedPath(to);
+  let match = useMatch({ path: resolved.pathname, end: true });
+
+  return (
+    <Link className={`${match ? "underline" : "text-slate-400"}`} to={to}>
+      {children}
+    </Link>
+  );
+};
+
+export default CustomIcon;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.render(
   <React.StrictMode>
