236977f88b8c079414953bd86bc0c96c2e570662
nort4n <trikkster@gmail.com>
2022-03-31 14:47:29 +0200

    added color inheritance to icon components

--- frontend/src/components/icons/grab-icon.tsx
+++ frontend/src/components/icons/grab-icon.tsx
@@ -1,27 +1,52 @@
-import { createIcon } from "@chakra-ui/icons"
+import { createIcon } from "@chakra-ui/icons";
 
 export const Icon = createIcon({
-    displayName: "GrabIcon",
-    viewBox: "0 0 15 25",
-    path: (
-        <>
-        <g filter="url(#filter0_d_34_448)">
-        <circle cx="7.5" cy="3.5" r="1.5" fill="#513131"/>
-        <circle cx="7.5" cy="8.5" r="1.5" fill="#513131"/>
-        <circle cx="7.5" cy="13.5" r="1.5" fill="#513131"/>
-        </g>
-        <defs>
-        <filter id="filter0_d_34_448" x="0" y="0" width="15" height="25" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-        <feOffset dy="4"/>
-        <feGaussianBlur stdDeviation="3"/>
-        <feComposite in2="hardAlpha" operator="out"/>
-        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_34_448"/>
-        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_34_448" result="shape"/>
+  displayName: "GrabIcon",
+  viewBox: "0 0 15 25",
+  path: (
+    <>
+      <g filter="url(#filter0_d_34_448)">
+        <circle cx="7.5" cy="3.5" r="1.5" fill="currentColor" />
+        <circle cx="7.5" cy="8.5" r="1.5" fill="currentColor" />
+        <circle cx="7.5" cy="13.5" r="1.5" fill="currentColor" />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_34_448"
+          x="0"
+          y="0"
+          width="15"
+          height="25"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="3" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_34_448"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_34_448"
+            result="shape"
+          />
         </filter>
-        </defs>
-        </>
-    )
-})
+      </defs>
+    </>
+  ),
+});
