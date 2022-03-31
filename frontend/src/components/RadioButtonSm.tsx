525147667962a0efc5e0ed5e2fcf46206d127f0d
Dean Wallflower <dean@dean.fritz.box>
2022-03-31 12:35:38 +0200

    checkboxes kinda work

--- frontend/src/components/RadioButtonSm.tsx
+++ frontend/src/components/RadioButtonSm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import api from "../../src/utils/api";
 import { Recipe } from "../../src/utils/types";
 
@@ -7,12 +7,13 @@ type Props = {
 }
 
 const RadioButtonSm = ({allergen}: Props) => {
-
+  const [checked, setChecked] = useState(false)
     return (
             <div className="form-check rounded-lg shadow-drop bg-ricewhite py-4">
               <label className="form-check-label flex justify-around w-full text-center text-darkbrown tracking-wide" htmlFor="flexCheckDefault">
-              <input className="form-check-input appearance-none h-[22px] w-[22px] ml-4 rounded-full bg-green checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 cursor-pointer shadow-inner" type="checkbox" value="" id="flexCheckDefault">
-                  </input>
+              <div className="form-check-input appearance-none h-[22px] w-[22px] ml-4 rounded-full bg-green checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 cursor-pointer shadow-inner flex justify-center items-center" id="flexCheckDefault" onClick={() => setChecked(!checked)}>
+                {checked && 'x'}
+                  </div>
                 <span className="grow">{allergen}</span>
               </label>
                   </div>
