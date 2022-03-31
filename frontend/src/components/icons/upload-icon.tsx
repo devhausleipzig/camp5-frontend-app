b1a3048f56f15b3fcbb2f22278e73d4916f8f7d1
nort4n <trikkster@gmail.com>
2022-03-31 16:13:49 +0200

    added missing icons as components

---
+++ frontend/src/components/icons/upload-icon.tsx
@@ -0,0 +1,15 @@
+import { createIcon } from "@chakra-ui/icons";
+
+export const Icon = createIcon({
+  displayName: "PreferencesIcon",
+  viewBox: "0 0 20 20",
+  path: (
+    <>
+      <path
+        fill="none"
+        stroke="currentColor"
+        d="M3.333 5H1.667v11.667c0 .916.75 1.666 1.666 1.666H15v-1.666H3.333V5zm13.334-3.333h-10C5.75 1.667 5 2.417 5 3.333v10C5 14.25 5.75 15 6.667 15h10c.916 0 1.666-.75 1.666-1.667v-10c0-.916-.75-1.666-1.666-1.666zm0 11.666h-10v-10h10v10zm-5.834-1.666H12.5v-2.5H15V7.5h-2.5V5h-1.667v2.5h-2.5v1.667h2.5v2.5z"
+      />
+    </>
+  ),
+});
