b2858e0b3f2a2f47cd78ed7b6eeac0363666d4e3
Dean Wallflower <dean@dean.fritz.box>
2022-03-31 14:27:01 +0200

    icon fix?

--- frontend/src/components/RadioButtonSm.tsx
+++ frontend/src/components/RadioButtonSm.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import api from "../../src/utils/api";
 import { Recipe } from "../../src/utils/types";
+import { Icon as CloseIcon } from "./icons/close-icon"
 
 type Props = {
   allergen: string
@@ -10,11 +11,11 @@ const RadioButtonSm = ({allergen}: Props) => {
   const [checked, setChecked] = useState(false)
     return (
             <div className="form-check rounded-lg shadow-drop bg-ricewhite py-4">
-              <label className="form-check-label flex justify-around w-full text-center text-darkbrown tracking-wide" htmlFor="flexCheckDefault">
+              <label className="form-check-label flex justify-around w-full text-center tracking-wide text-ricewhite" htmlFor="flexCheckDefault">
               <div className="form-check-input appearance-none h-[22px] w-[22px] ml-4 rounded-full bg-green checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 cursor-pointer shadow-inner flex justify-center items-center" id="flexCheckDefault" onClick={() => setChecked(!checked)}>
-                {checked && 'x'}
+                {checked && <CloseIcon />}
                   </div>
-                <span className="grow">{allergen}</span>
+                <span className="grow text-darkbrown">{allergen}</span>
               </label>
                   </div>
     )
