a213d80c899d63e1fe53aa8663d448e3c60e176d
JonathanWagne <jonathan_wagner@hotmail.de>
2022-03-31 09:52:14 +0200

    Clean up App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,22 @@
 import { useEffect, useState } from "react";
-import { createRoutesFromChildren } from "react-router-dom";
 import RecipeCard from "./components/card";
-import RecipeCardFront from "./components/recipecard-front";
-import RecipeCardBack from "./components/recipecard-back";
-import Searchbar from "./components/searchbar";
-import logo from "./logo.svg";
 import { getRecipes } from "./utils/fetchRecipes";
 import type { Recipe } from "./utils/types";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [allRecipes, setAllRecipes] = useState<Recipe[]>([]);
 
-  async function fetchAllRecipes(): Promise<Recipe[]> {
+  async function fetchAllRecipes(): Promise<void> {
     const response: Recipe[] = await getRecipes();
     setAllRecipes(response);
-    return allRecipes;
   }
+
   useEffect(() => {
     fetchAllRecipes();
   }, []);
 
   return (
     <div className="App">
-      {allRecipes.length && (
-        <>
-          <RecipeCardFront
-            img={allRecipes[0].picture}
-            recipeName={allRecipes[0].recipeName}
-          />
-          <RecipeCardBack
-            backSide="info"
-            contentInfo={allRecipes[0].category}
-            recipeName={allRecipes[0].recipeName}
-          />
-        </>
-      )}
       {allRecipes.length && (
         <RecipeCard
           recipe={allRecipes[0]}
@@ -44,37 +25,6 @@ function App() {
           contentSteps={[]}
         />
       )}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-green-500">Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
     </div>
   );
 }
