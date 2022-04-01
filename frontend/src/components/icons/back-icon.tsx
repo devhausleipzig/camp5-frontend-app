4854f42917bad2621f14451e3f9269c8ee37bc5c
nort4n <trikkster@gmail.com>
2022-04-01 14:34:21 +0200

    added back button icon component

---
+++ frontend/src/components/icons/back-icon.tsx
@@ -0,0 +1,18 @@
+import { createIcon } from "@chakra-ui/icons";
+
+export const Icon = createIcon({
+  displayName: "MaximizeIcon",
+  viewBox: "0 0 20 20",
+  path: (
+    <>
+      <path
+        fill="currentColor"
+        d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"
+      ></path>
+      <path
+        fill="currentColor"
+        d="m237.248 512l265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"
+      ></path>
+    </>
+  ),
+});
